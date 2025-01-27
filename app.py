d6cab676f2aef8e686678635a184f6769509e609
Anne Frahm <64586643+Afrhm@users.noreply.github.com>
2025-01-27 12:36:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -63,13 +63,15 @@ with col2:
 url_paper = "https://github.com/PERSIMUNE/DAIMS/blob/main/DAIMS_DatasetName_DDMMYYYY.md"
 st.write("For instructions of use please view [Datasheets for AI and medical datasets (DAIMS)](%s)" % url_paper) #change when published!
 
-st.write("To reference this tool, please cite: @article{marandi2025daims,\n \
+citation = "To reference this tool, please cite: @article{marandi2025daims,\n" \
 + "title={Datasheets for AI and medical datasets (DAIMS): a data validation and documentation framework before machine learning analysis in medical research}, \n" \
 + "author={Ramtin Zargari Marandi and Anne Svane Frahm and Maja Milojevic}, \n" \
 + "journal={arXiv preprint arXiv:2501.14094}0,\n" \
 + "year={2025}, \n" \
-+ "url={https://arxiv.org/abs/2501.14094} \n \
-+ "}\n")
++ "url={https://arxiv.org/abs/2501.14094} \n" \
++ "}\n"
+
+st.write(citation)
 
 #upload widgets for data and dd
 st.subheader("Upload your Data file:")
